2a349fa058d2c3c6b322e2d7f7151524f5fcaf3e
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-02-18 11:57:53 -0800

    Update my counter component to include button hover and updated sizing styles in the fast template

--- packages/create-vite/template-fast/src/my-counter.js
+++ packages/create-vite/template-fast/src/my-counter.js
@@ -9,38 +9,45 @@ export class MyCounter extends FASTElement {
       <button @click="${(x) => x.count++}">+</button>
     `,
     styles: css`
-      * {
-        font-size: 200%;
+      button, span {
+        font-family: inherit;
+        font-size: 32px;
       }
-
+    
       span {
         width: 4rem;
         display: inline-block;
         text-align: center;
       }
-
+    
       button {
+        box-sizing: border-box;
         display: inline-flex;
         justify-content: center;
         align-items: center;
-        width: 64px;
-        height: 64px;
         border: 1px solid transparent;
         border-radius: 4px;
+        width: 42px;
+        height: 42px;
         background-color: #c01754;
         color: white;
       }
-
+    
+      button:hover {
+        cursor: pointer;
+        background-color: #da1a5f;
+      }
+    
       button:focus-visible {
         border 1px solid #000;
         box-shadow: #707070 0px 0px 0px 1px inset, #FFFFFF 0px 0px 0px 3px inset;
+        outline: none;
       }
     `
   }
 
   constructor() {
     super()
-
     this.count = 0
   }
 }
